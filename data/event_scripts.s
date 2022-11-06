bf9fb833ed19dbaa8d8257a9e693cb866f42e33c
barym <f.soelle@hotmail.de>
2022-11-06 15:48:36 +0100

    Change Route 102 name to Route 302

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -118,7 +118,7 @@ gStdScripts_End::
 	.include "data/maps/VerdanturfTown/scripts.inc"
 	.include "data/maps/PacifidlogTown/scripts.inc"
 	.include "data/maps/Route301/scripts.inc"
-	.include "data/maps/Route102/scripts.inc"
+	.include "data/maps/Route302/scripts.inc"
 	.include "data/maps/Route103/scripts.inc"
 	.include "data/maps/Route104/scripts.inc"
 	.include "data/maps/Route105/scripts.inc"
